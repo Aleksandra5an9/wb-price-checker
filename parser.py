341856d7c43ad72f5811ae9f9ff7622fa349ad89
Aleksandra5an9 <119672860+Aleksandra5an9@users.noreply.github.com>
2025-05-08 19:47:11 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,15 @@
 # parser.py
+import requests
 
 def get_price(wb_id):
-    # Здесь должна быть логика получения цены с Wildberries
-    # Пока просто возвращаем фиктивную цену
-    return 999
+    url = f"https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=0&nm={wb_id}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            return data['data']['products'][0]['priceU'] // 100  # Цена в рублях
+        except (IndexError, KeyError):
+            return None
+    else:
+        return None
