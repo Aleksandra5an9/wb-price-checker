9e1e2d57d8f14202b58089acec0e38c5552ca2d6
Aleksandra5an9 <119672860+Aleksandra5an9@users.noreply.github.com>
2025-05-12 18:49:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import os
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
+def get_real_price(nmID):
+    url = f'https://card.wb.ru/cards/detail?nm={nmID}'
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f"Ошибка при запросе: {response.status_code}")
+        return
 
-url = "https://discounts-prices-api.wildberries.ru/api/v2/list/goods/filter"
-headers = {
-    "Authorization": f"Bearer {API_KEY}"
-}
-params = {
-    "filterNmID": 260800583,
-    "limit": 1
-}
+    data = response.json()
 
-response = requests.get(url, headers=headers, params=params)
+    try:
+        product = data['data']['products'][0]
+        price = product['priceU'] / 100
+        sale_price = product['salePriceU'] / 100
+        discount = product.get('discount', round((1 - sale_price / price) * 100))
+        
+        print(f"Арт: {nmID}")
+        print(f"Цена без скидки: {price:.2f} ₽")
+        print(f"Цена со скидкой: {sale_price:.2f} ₽")
+        print(f"Итоговая скидка: {discount}%")
+    except (IndexError, KeyError) as e:
+        print("Ошибка в структуре данных:", e)
 
-print(f"Status code: {response.status_code}")
-print("Response:", response.json())
+# Пример использования
+get_real_price(260800583)
